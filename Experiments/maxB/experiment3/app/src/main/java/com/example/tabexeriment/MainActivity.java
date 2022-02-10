da4423c9969c9c541bbddf34c05a0b0027d3c707
Max <mabromet@gmail.com>
2022-02-09 20:37:02 -0600

    got demo working

--- Experiments/maxB/experiment3/app/src/main/java/com/example/tabexeriment/MainActivity.java
+++ Experiments/maxB/experiment3/app/src/main/java/com/example/tabexeriment/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.tabexeriment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TabLayout tabslayout;
+    private TabLayout tabsLayout;
     private ViewPager2 pager;
     private TabsAdapter adapter;
 
@@ -19,5 +23,11 @@ public class MainActivity extends AppCompatActivity {
         pager = findViewById(R.id.pager);
         adapter = new TabsAdapter(this);
         pager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabsLayout, pager, (tab, position) -> {
+            if (position == 0) tab.setText("Contacts");
+            else if (position == 1) tab.setText("Favourites");
+            else tab.setText("Settings");
+        }).attach();
     }
 }
