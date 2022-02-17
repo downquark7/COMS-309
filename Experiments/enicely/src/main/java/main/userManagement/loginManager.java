5259728940953e9c667f3409e653c2c12912ef88
enicely <enicely@iastate.edu>
2022-02-16 23:59:23 -0600

    added (random) user id

--- Experiments/enicely/src/main/java/main/userManagement/loginManager.java
+++ Experiments/enicely/src/main/java/main/userManagement/loginManager.java
@@ -6,20 +6,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.Random;
 
 @RestController
 public class loginManager
 {
-    private HashMap<String, loginData> onlineUsers = new HashMap<>();
+    //TODO: replace hashmap value with userData class once implemented
+    private HashMap<Integer, String> onlineUsers = new HashMap<>();
 
     @GetMapping("/onlineUsers")
-    public HashMap<String, loginData> getOnlineUsers() {
+    public HashMap<Integer, String> getOnlineUsers() {
         return onlineUsers;
     }
 
     @PostMapping("/login")
-    public String loginUser(@RequestBody loginData data) {
-        onlineUsers.put(data.getUsername(), data);
-        return data.getUsername();
+    public Integer loginUser(@RequestBody loginData data) {
+        Integer id = new Random().nextInt();
+        onlineUsers.put(id, data.getUsername());
+        return id;
     }
 }
