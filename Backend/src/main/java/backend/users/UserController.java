b4c0e7bb60e44f52a20763d74d0bc6d0a645fdc4
enicely <enicely@iastate.edu>
2022-02-22 23:24:39 -0600

    progress on user controller

--- Backend/src/main/java/backend/users/UserController.java
+++ Backend/src/main/java/backend/users/UserController.java
@@ -1,17 +1,14 @@
 package backend.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
 @RestController
 public class UserController
 {
-//    private HashMap<Integer, User> onlineUsers = new HashMap<>();
+    private HashMap<Integer, User> onlineUsers = new HashMap<>();
 
     @Autowired
     UserRepository userRepository;
@@ -24,5 +21,47 @@ public class UserController
         else
             return null;
         return user;
+    }
+
+    @PutMapping("/user/manage")
+    public User manageUser(@RequestBody User user)
+    {
+        userRepository.save(user);
+        return user;
+    }
+
+    @PostMapping("/user/login")
+    public User loginUser(@RequestBody User user)
+    {
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+
+        if (userFromDb != null)
+        {
+            if (!user.getAuthenticationMethod().equals(userFromDb.getAuthenticationMethod()))
+                return null;
+            if (!user.getAuthenticationData().equals(userFromDb.getAuthenticationData()))
+                return null;
+
+        } else
+        {
+            return null;
+        }
+
+        onlineUsers.put(user.getId(), user);
+
+        return userFromDb;
+    }
+
+    @GetMapping("/onlineUsers")
+    public HashMap<Integer, User> getOnlineUsers()
+    {
+        return onlineUsers;
+    }
+
+    @PostMapping("/user/logout")
+    public User logout(@RequestBody User user) {
+        User userFromHashmap = onlineUsers.get(user.getId());
+        onlineUsers.remove(user.getId());
+        return userFromHashmap;
     }
 }
