b6f4cf3787768fd65a293f24af9974cb80c93381
Max <mabromet@gmail.com>
2022-03-04 22:23:04 -0600

    moving stuff around and changing code

--- Frontend/FrontendApplication/app/src/main/java/com/example/classdifficultyfrontend/MainActivity.java
+++ Frontend/FrontendApplication/app/src/main/java/com/example/classdifficultyfrontend/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
     private RecyclerView classList;
     //JSONPlaceholder jsonPlaceholder;
+    private TabLayout tabsLayout;
 
 
 
