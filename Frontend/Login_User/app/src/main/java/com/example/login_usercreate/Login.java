51b974344ef05212278dedd4f81995b19c978249
SamRett <98674538+SamRett@users.noreply.github.com>
2022-03-05 23:05:40 -0600

    moved code ~interestingly, I found an merge error.

--- Frontend/Login_User/app/src/main/java/com/example/login_usercreate/Login.java
+++ Frontend/Login_User/app/src/main/java/com/example/login_usercreate/Login.java
@@ -2,13 +2,93 @@ package com.example.login_usercreate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Login extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView TextResult;
+    private RequestQueue queue;
+    private String text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+
+        //  TextResult = findViewById(R.id.text_view_result);
+        Button button = findViewById(R.id.button_parse);
+
+        //Button get_request_button=findViewById(R.id.button_parse);
+        TextResult = findViewById(R.id.text_view_result);
+
+        // queue = Volley.newRequestQueue(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getRequest();
+            }
+        });
+    }
+
+
+    private void getRequest() {
+        RequestQueue queue= Volley.newRequestQueue(MainActivity.this);
+        String url="http://coms-309-032.class.las.iastate.edu:8080/classes/MUSIC";
+        StringRequest stringRequest=new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                TextResult.setText(response);
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+                    JSONObject jsonObject =(jsonArray.getJSONObject(0));
+                    TextResult.setText("ID :"+jsonObject.getString("id"));
+                    TextResult.append("\n");
+                    TextResult.append("Semester Code :"+jsonObject.getString("semesterCode"));
+                    TextResult.append("\n");
+                    TextResult.append("Semester Year :"+jsonObject.getString("semesterYear"));
+                    TextResult.append("\n");
+                    TextResult.append("Department Code :"+jsonObject.getString("deptCode"));
+                    TextResult.append("\n");
+                    TextResult.append("Class Number :"+jsonObject.getString("classNumber"));
+
+                    text = jsonObject.toString();
+                    Intent intent = new Intent(getApplicationContext(), Second.class);
+                    intent.putExtra("message_key", text);
+                    startActivity(intent);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                    TextResult.setText("Failed to get Data");
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                TextResult.setText(error.toString());
+            }
+        });
+
+        queue.add(stringRequest);
     }
 }
--- Frontend/Login_User/app/src/main/java/com/example/login_usercreate/User_Create.java
+++ Frontend/Login_User/app/src/main/java/com/example/login_usercreate/User_Create.java
@@ -1,14 +1,114 @@
 package com.example.login_usercreate;
 
+import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Parcelable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class User_Create extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.StringWriter;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView TextResult;
+    private RequestQueue queue;
+    private String text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_create);
+        setContentView(R.layout.activity_main);
+
+        TextResult = findViewById(R.id.text_view_result);
+        Button button = findViewById(R.id.button_parse);
+
+        // queue = Volley.newRequestQueue(this);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                postRequest();
+            }
+        });
     }
+
+
+    private void postRequest() {
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+
+        String url = "http://coms-309-032.class.las.iastate.edu:8080/user/create";
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("username", "Pli77e" + "123");
+            jsonBody.put("authenticationMethod", "pliez");
+            jsonBody.put("authenticationData", "pilez");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        final String requestBody = jsonBody.toString();
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    TextResult.setText(response);
+                    JSONObject jsonObject = new JSONObject(response);
+
+                    TextResult.setText("Username: " + jsonObject.getString("username"));
+                    TextResult.append("\n");
+                    TextResult.append("Authentication Method: " + jsonObject.getString("authenticationMethod"));
+                    TextResult.append("\n");
+                    TextResult.append("Authentication Data: " + jsonObject.getString("authenticationData"));
+
+                    text = jsonObject.toString();
+                    Intent intent = new Intent(getApplicationContext(), Second.class);
+                    intent.putExtra("message_key", text);
+                    startActivity(intent);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                TextResult.setText(error.toString());
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                return requestBody.getBytes();
+            }
+        };
+        queue.add(request);
+
+    }
+
+
+
 }
