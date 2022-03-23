75375349f62f6e92fbfb1a8e02582c4f1a56d401
enicely <enicely@iastate.edu>
2022-03-22 19:01:13 -0500

    added department codes added class data collection method

--- Frontend/FrontendApplication/app/src/main/java/volley/VolleyConfig.java
+++
@@ -1,5 +0,0 @@
-package volley;
-
-public class VolleyConfig {
-    public static String baseURL = "http://coms-309-032.class.las.iastate.edu:8080";
-}
--- Frontend/FrontendApplication/app/src/test/java/volley/ClassControllerTest.java
+++ Frontend/FrontendApplication/app/src/test/java/volley/ClassControllerTest.java
@@ -1,16 +1,18 @@
 package volley;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
 import junit.framework.TestCase;
 
 import java.io.IOException;
+
+import controller.ClassController;
+import controller.ClassData;
+import controller.DepartmentCodes;
 
 public class ClassControllerTest extends TestCase {
 
     public void testGetAClass() throws IOException {
         ClassController classController = new ClassController();
-        ClassData classData = classController.getAClass();
+        ClassData classData = classController.getAClass("MUSIC","102");
         assertEquals(classData.getClassNumber(), "102");
 //        assertEquals(1, 1);
     }
