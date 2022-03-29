7c6fd29d702e095d387c012cc00adb55d2ea1fd3
Max <mabromet@gmail.com>
2022-03-28 22:07:57 -0500

    got rid of all of the pregenerated code in ScheduleActivity.java

--- Frontend/FrontendApplication/app/src/main/java/com/example/classdifficultyfrontend/ScheduleActivity.java
+++ Frontend/FrontendApplication/app/src/main/java/com/example/classdifficultyfrontend/ScheduleActivity.java
@@ -17,35 +17,4 @@ import com.example.classdifficultyfrontend.databinding.ActivityScheduleBinding;
 
 public class ScheduleActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityScheduleBinding binding;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        binding = ActivityScheduleBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_schedule);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_schedule);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
 }
