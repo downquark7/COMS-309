5ad26a59e6453ca5b86ef935a5dfc9d7677b3dc8
SamRett <98674538+SamRett@users.noreply.github.com>
2022-04-02 20:50:49 -0500

    app now shows text from spinner in textview

--- Frontend/FrontendApplication/ClassList/app/src/main/java/com/example/classlist/MainActivity.java
+++ Frontend/FrontendApplication/ClassList/app/src/main/java/com/example/classlist/MainActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +17,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView text = (TextView) findViewById(R.id.textView);
         Spinner choose = (Spinner) findViewById(R.id.spinner);
 
         choose.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String t = choose.getSelectedItem().toString();
 
-                Toast.makeText(MainActivity.this, parent.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+                text.setText(t);
             }
 
             @Override
