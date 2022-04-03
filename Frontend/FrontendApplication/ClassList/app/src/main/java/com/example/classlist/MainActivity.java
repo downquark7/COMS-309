5bcd098f8e61d7536f5e27789ffdec2a2da83ca7
SamRett <98674538+SamRett@users.noreply.github.com>
2022-04-02 20:27:55 -0500

    testing

--- Frontend/FrontendApplication/ClassList/app/src/main/java/com/example/classlist/MainActivity.java
+++ Frontend/FrontendApplication/ClassList/app/src/main/java/com/example/classlist/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 //import departmentCodes.DepartmentCodes;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +16,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner choose = (Spinner) findViewById(R.id.spinner);
+
+        choose.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                //  Do Something
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
