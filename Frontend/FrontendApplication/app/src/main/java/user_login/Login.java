0b7444ec73b592f8163e292cc7327fd77c5fd1e1
SamRett <98674538+SamRett@users.noreply.github.com>
2022-04-04 09:50:32 -0500

    added internet permission for volley, login works

--- Frontend/FrontendApplication/app/src/main/java/user_login/Login.java
+++ Frontend/FrontendApplication/app/src/main/java/user_login/Login.java
@@ -34,7 +34,7 @@ public class Login extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login3);
 
 
         TextResult = findViewById(R.id.textUser);
