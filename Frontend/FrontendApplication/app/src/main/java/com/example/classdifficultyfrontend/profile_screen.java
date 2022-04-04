a8fd0aee7165c8be79c64d4e6058f5335e4a9cad
SamRett <98674538+SamRett@users.noreply.github.com>
2022-04-04 14:48:29 -0500

    added functional (but basic) profile screen)

--- Frontend/FrontendApplication/app/src/main/java/com/example/classdifficultyfrontend/profile_screen.java
+++ Frontend/FrontendApplication/app/src/main/java/com/example/classdifficultyfrontend/profile_screen.java
@@ -1,6 +1,11 @@
 package com.example.classdifficultyfrontend;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import userController.User;
+import userController.Schedule;
+import userController.UserController;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +14,10 @@ public class profile_screen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_screen);
+
+//        ArrayAdapter adapter = ArrayAdapter.createFromResource(this, R.array.profile_screen, R.layout.spinner_item);
+//        spinner.setAdapter(adapter);
 
 
     }
