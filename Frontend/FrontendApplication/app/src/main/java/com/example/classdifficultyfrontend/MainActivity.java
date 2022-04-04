96ecb1c7cb4f1804d615effe32878c8adedbfd5c
SamRett <98674538+SamRett@users.noreply.github.com>
2022-04-04 09:57:22 -0500

    need to switch branches to get implementations

--- Frontend/FrontendApplication/app/src/main/java/com/example/classdifficultyfrontend/MainActivity.java
+++ Frontend/FrontendApplication/app/src/main/java/com/example/classdifficultyfrontend/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                startActivity(new Intent(MainActivity.this, Login.class));
+                startActivity(new Intent(MainActivity.this, updated_class_list.class));
             }
         });
         classListButton = (Button) findViewById(R.id.button_go_to_classlist);
