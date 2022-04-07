812276459dcbd683a00465039f7dd72e988edb87
SamRett <98674538+SamRett@users.noreply.github.com>
2022-04-07 18:21:05 -0500

    adding in some logic for sendinf email + password

--- Frontend/FrontendApplication/app/src/main/java/com/example/classdifficultyfrontend/profile_screen.java
+++ Frontend/FrontendApplication/app/src/main/java/com/example/classdifficultyfrontend/profile_screen.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import userController.User;
@@ -12,6 +13,17 @@ import userController.Schedule;
 import userController.UserController;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class profile_screen extends AppCompatActivity {
 
@@ -21,6 +33,12 @@ public class profile_screen extends AppCompatActivity {
         setContentView(R.layout.activity_profile_screen);
         Button button = findViewById(R.id.button);
         Spinner spin = findViewById(R.id.spinner);
+
+        EditText email;
+        EditText phone;
+
+        email = findViewById(R.id.Email);
+        phone = findViewById(R.id.Phone);
 
 
 
@@ -40,6 +58,55 @@ public class profile_screen extends AppCompatActivity {
     }
 
     private void sendData() {
-        
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        String url = "http://coms-309-032.class.las.iastate.edu:8080/user";
+
+        String UserEmail = email.getText().toString().trim();
+        String UserPhone = pass.getText().toString().trim();
+
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("username", Userstr);
+            jsonBody.put("authenticationMethod", Userpass);
+            jsonBody.put("authenticationData", Userpass);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        final String requestBody = jsonBody.toString();
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    TextResult.setText(response);
+                    JSONObject jsonObject = new JSONObject(response);
+
+                    TextResult.setText("Username: " + jsonObject.getString("username"));
+                    TextResult.append("\n");
+                    TextResult.append("Authentication Method: " + jsonObject.getString("authenticationMethod"));
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                TextResult.setText(error.toString());
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                return requestBody.getBytes();
+            }
+        };
+        queue.add(request);
+
     }
 }
