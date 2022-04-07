c9928f73b4778cdc83cda699ad67e1dbc9912d59
SamRett <98674538+SamRett@users.noreply.github.com>
2022-04-07 18:14:11 -0500

    added field for data to transfer on  click of button

--- Frontend/FrontendApplication/app/src/main/java/com/example/classdifficultyfrontend/profile_screen.java
+++ Frontend/FrontendApplication/app/src/main/java/com/example/classdifficultyfrontend/profile_screen.java
@@ -1,7 +1,10 @@
 package com.example.classdifficultyfrontend;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import userController.User;
@@ -16,8 +19,9 @@ public class profile_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_screen);
-
+        Button button = findViewById(R.id.button);
         Spinner spin = findViewById(R.id.spinner);
+
 
 
 
@@ -27,7 +31,15 @@ public class profile_screen extends AppCompatActivity {
         adapter.setDropDownViewResource(R.layout.spinner_xml_layout);
         spin.setAdapter(adapter);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendData();
+            }
+        });
+    }
 
-
+    private void sendData() {
+        
     }
 }
