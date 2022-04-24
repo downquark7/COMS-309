c274731fa60cf9c43680a94f63bfed158bc7877c
SamRett <98674538+SamRett@users.noreply.github.com>
2022-04-23 23:36:43 -0500

    added course matcher page

---
+++ Frontend/FrontendApplication/app/src/main/java/com/example/classdifficultyfrontend/Course_matcher.java
@@ -0,0 +1,14 @@
+package com.example.classdifficultyfrontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Course_matcher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_matcher);
+    }
+}
