fef21b262c2960f485471c77763b339023880faa
SamRett <98674538+SamRett@users.noreply.github.com>
2022-04-30 22:02:52 -0500

    made the course matcher look better, more demo-able

--- Frontend/FrontendApplication/app/src/main/java/com/example/classdifficultyfrontend/Course_matcher.java
+++ Frontend/FrontendApplication/app/src/main/java/com/example/classdifficultyfrontend/Course_matcher.java
@@ -3,12 +3,26 @@ package com.example.classdifficultyfrontend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Course_matcher extends AppCompatActivity {
+
+    Spinner Spinner;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_matcher);
+
+        Spinner = (Spinner)findViewById(R.id.spinner);
+
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,
+                R.array.Classes_2, R.layout.spinner);
+
+        adapter.setDropDownViewResource(R.layout.spinner);
+        Spinner.setAdapter(adapter);
     }
 }
