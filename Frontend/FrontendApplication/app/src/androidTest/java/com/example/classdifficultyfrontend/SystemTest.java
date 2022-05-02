c9a718396f65648c72c5b7ae6b01493862084be6
SamRett <98674538+SamRett@users.noreply.github.com>
2022-05-01 22:52:27 -0500

    added one test, need to potentially fix later

--- Frontend/FrontendApplication/app/src/androidTest/java/com/example/classdifficultyfrontend/SystemTest.java
+++ Frontend/FrontendApplication/app/src/androidTest/java/com/example/classdifficultyfrontend/SystemTest.java
@@ -1,4 +1,22 @@
 package com.example.classdifficultyfrontend;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 public class SystemTest {
+
+    public void test_login() throws Exception{
+        onView(withId(R.id.Username_input)).perform(typeText("testcreate"),closeSoftKeyboard());
+        onView(withId(R.id.Pass_input)).perform(typeText("create"),closeSoftKeyboard());
+        onView(withId(R.id.button)).perform(click());
+
+        onView(withId(R.id.textUser)).check(matches(withText("Error")));
+    }
+
+
 }
