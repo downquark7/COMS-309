26fb3ceafd2ebe2ac0df6cae935b800c934e9026
SamRett <98674538+SamRett@users.noreply.github.com>
2022-05-02 09:06:28 -0500

    added test stuff

--- Frontend/FrontendApplication/app/src/androidTest/java/com/example/classdifficultyfrontend/SystemTest.java
+++ Frontend/FrontendApplication/app/src/androidTest/java/com/example/classdifficultyfrontend/SystemTest.java
@@ -1,5 +1,16 @@
 package com.example.classdifficultyfrontend;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+import androidx.test.filters.LargeTest;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.rule.ActivityTestRule;
+
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -7,9 +18,18 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.StringEndsWith.endsWith;
 
+@RunWith(AndroidJUnit4ClassRunner.class)
+@LargeTest
 public class SystemTest {
 
+    private static final int SIMULATED_DELAY_MS = 500;
+
+    @Rule   // needed to launch the activity
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
     public void test_login() throws Exception{
         onView(withId(R.id.Username_input)).perform(typeText("testcreate"),closeSoftKeyboard());
         onView(withId(R.id.Pass_input)).perform(typeText("create"),closeSoftKeyboard());
