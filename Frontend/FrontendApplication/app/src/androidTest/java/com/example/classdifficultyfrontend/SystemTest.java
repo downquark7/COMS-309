bbe8c0570386bc0e5c50bd1a5ddff53415b850ff
SamRett <98674538+SamRett@users.noreply.github.com>
2022-05-03 14:31:50 -0500

    added to test for login, should be better now

--- Frontend/FrontendApplication/app/src/androidTest/java/com/example/classdifficultyfrontend/SystemTest.java
+++ Frontend/FrontendApplication/app/src/androidTest/java/com/example/classdifficultyfrontend/SystemTest.java
@@ -33,6 +33,7 @@ public class SystemTest {
     public void test_login() throws Exception{
         onView(withId(R.id.Username_input)).perform(typeText("testcreate"),closeSoftKeyboard());
         onView(withId(R.id.Pass_input)).perform(typeText("create"),closeSoftKeyboard());
+        onView(withId(R.id.Pass_input)).perform(typeText("create"),closeSoftKeyboard());
         onView(withId(R.id.button)).perform(click());
 
         onView(withId(R.id.textUser)).check(matches(withText(" ")));
